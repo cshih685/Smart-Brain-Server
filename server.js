bc4e1f08f5905f75885e067d584f6a46bd1ded58
cshih685 <32198222+leehon3322@users.noreply.github.com>
2019-06-13 17:25:59 -0400

    adding port as depending on environment

--- server.js
+++ server.js
@@ -68,8 +68,8 @@ bcrypt.genSalt(saltRounds, (err, salt) => {
 // 	]
 // }
 
-app.listen(3000, ()=>{
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{ //if we can run on heroku then run on it, instead, run on 3000
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 app.get('/', (req, res) => {
